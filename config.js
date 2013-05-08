1df635b864e82a6ac803da435df1403fc90b632f
Stuart <stuart@vericode.co.uk>
2013-05-08 14:42:10 +0200

    Create config.js
    ckeditor/config.js for custom dropdown example

---
+++ config.js
@@ -0,0 +1,26 @@
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+// Shortened config for demo example
+
+CKEDITOR.editorConfig = function( config ) {
+	// The toolbar groups arrangement, optimized for two toolbar rows.
+	config.toolbarGroups = [
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'links' },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ] },
+		{ name: 'styles' },
+
+    // Add strinsert plugin
+		{ name: 'strinsert' }
+
+	];
+
+  // Register the strinsert plugin
+	config.extraPlugins = 'strinsert';
+
+};
